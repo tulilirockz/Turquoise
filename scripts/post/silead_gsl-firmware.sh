959e7ae588beef58056bc32cb5f09c225c1eca42
Tulili <tulilirockz.pub+git@gmail.com>
2023-05-26 13:12:14 -0300

    feat: add touchscreen driver support

---
+++ scripts/post/silead_gsl-firmware.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+git clone https://github.com/onitake/gsl-firmware /tmp/firmware
+mkdir -p /usr/lib/firmware/silead
+cp -f /tmp/firmware/firmware/positivo/c464c/*.fw /usr/lib/firmware/silead
