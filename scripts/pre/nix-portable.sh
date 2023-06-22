c655e1ed2d76826f1ef6ad0c3a3292c4fa4afedc
Tulili Blossom <tulilirockz.pub+git@gmail.com>
2023-06-22 11:03:31 -0300

    revert: remove nix portable scripts and use raw nix

--- scripts/pre/nix-portable.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -euo pipefail
-wget -O /usr/bin/nix-portable https://github.com/DavHau/nix-portable/releases/download/v009/nix-portable
-chmod +x /usr/bin/nix-portable
-chmod +x /usr/bin/nix
