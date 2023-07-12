e386ac95a42d896e8a7eb9523c90c3309b5b91f1
Tulili <tulilirockz.pub+git@gmail.com>
2023-07-12 02:37:38 -0300

    fix: add rtl8xxxu workaround to networkmanager

--- scripts/post/cockpit-fix.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -euo pipefail
-# Fixes cockpit "Internal login error" thing!
-sed -i 's/is():where()/is(*):where(*)/' /usr/share/cockpit/static/login.js
--- scripts/pre/networkmanager-realtek.sh
+++ scripts/pre/networkmanager-realtek.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 set -oue pipefail
 echo -e "[device]\nwifi.backend=iwd" | tee -a /etc/NetworkManager/conf.d/iwd.conf
+echo -e "[device]\nwifi.scan-rand-mac-address=no" | tee -a /etc/NetworkManager/conf.d/rtl8xxxu_workaround.conf
